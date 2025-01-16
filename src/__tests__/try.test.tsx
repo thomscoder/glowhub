b40b1d2f27ff1458577030f96ecbeb7b2b6b65c9
thomscoder <moyelysian@icloud.com>
2025-01-16 13:09:39 +0000

    create check run

---
+++ src/__tests__/try.test.tsx
@@ -0,0 +1,17 @@
+describe('Basic Test Suite', () => {
+    test('adds two numbers correctly', () => {
+      const add = (a: number, b: number) => a + b;
+      expect(add(2, 2)).toBe(4);
+    });
+  
+    test('concatenates strings', () => {
+      const concat = (a: string, b: string) => a + b;
+      expect(concat('Hello, ', 'World!')).toBe('Hello, World!');
+    });
+  
+    // This test will fail to demonstrate PR blocking
+    test('this test will fail', () => {
+      const shouldBeTrue = false;
+      expect(shouldBeTrue).toBe(false);
+    });
+  });
